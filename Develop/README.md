618fbd37042674c169b866c1ee3c3050774b5851
Saadia9 <amzilsaadia9@gmail.com>
2024-08-12 01:06:43 -0600

    First commit Readme Generator

---
+++ Develop/README.md
@@ -0,0 +1,33 @@
+# Best Readme File Generator
+
+## Description
+Best Readme file generator is a command line app that helps developers generate a readme file for their project
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Clone the repository https://github.com/Saadia9/Readme-file-generator to your local machine using your terminal, open the directory and use npm i command to install the dependencies required, once done run the comand node index.js and answer the prompt questions.
+
+## Usage
+Run the command node  index.js Answer the series of questions the will be prompted  about your project. Once done, a nice README.md file will be generated in your project directory 
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+Clone the repository > create your branch and commit and push the changes you want to make
+
+## Tests
+To test the application use the following command : npm test
+
+## Questions
+If you have any questions, you can reach me at:
+
+- GitHub: [Saadia9](https://github.com/Saadia9)
+- Email: amzilsaadia9@gmail.com
---
+++ Develop/index.js
@@ -0,0 +1,106 @@
+import inquirer from 'inquirer';
+import { writeFile } from 'fs/promises';
+
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, you can reach me at:
+
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
+`;
+}
+
+async function writeToFile(fileName, data) {
+  try {
+    await writeFile(fileName, data);
+    console.log('README.md has been generated!');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdown = generateMarkdown(answers);
+    writeToFile('README.md', markdown);
+  });
+}
+
+init();
